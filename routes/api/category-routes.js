fa3129baec193978eba9f4b4c094438638119cff
r134x7 <r134x7@gmx.com>
2022-05-21 16:35:25 +1000

    added category delete request

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -54,8 +54,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deleteData) {
+      res.status(404).json({ message: "404 error "});
+      return;
+    }
+
+    res.status(200).json(deleteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
