83d95e657964c75cdb5e717a34f583eb3163c2a6
r134x7 <r134x7@gmx.com>
2022-05-21 18:42:33 +1000

    added delete request for tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,6 +62,22 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deleteData) {
+      res.status(404).json({ message: "404 error "});
+      return;
+    }
+
+    res.status(200).json(deleteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
