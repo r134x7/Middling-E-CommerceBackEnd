f63bbf4c9a57ac76dbf51c527448d70c610559ee
r134x7 <r134x7@gmx.com>
2022-05-21 18:23:56 +1000

    added post request for tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,15 +29,26 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  /* req.body structure
+    {
+	"tag_name": "something"
+     }
+  */
+  try {
+    const someData = await Tag.create(req.body);
+    res.status(200).json(someData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
 });
 
