cea63b03e36c99b73c80a0f7fcc9748641c8a20c
r134x7 <r134x7@gmx.com>
2022-05-21 18:53:26 +1000

    added delete request for product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -105,8 +105,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deleteData) {
+      res.status(404).json({ message: "404 error "});
+      return;
+    }
+
+    res.status(200).json(deleteData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
